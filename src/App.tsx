f4da82e31c764715fd60333eb97def75366f7c9f
maciek <macieq2048@gmail.com>
2022-01-26 23:14:14 +0100

    change project to work with github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import Game from "./views/Game";
-import { Routes, Route} from "react-router-dom";
+import {Routes, Route, BrowserRouter, HashRouter} from "react-router-dom";
 import WelcomePage from "./views/WelcomePage";
 import Leaderboard from "./views/Leaderboard";
 import {useState} from "react";
+
 
 function App() {
   const [playerName, setPlayerName] = useState("")
@@ -10,11 +11,15 @@ function App() {
 
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<WelcomePage playerName={playerName} setPlayerName={setPlayerName}/>} />
-        <Route path="game" element={<Game setScore={setScore}/>} />
-        <Route path="leaderboard" element={<Leaderboard username={playerName} score={score}/>} />
-      </Routes>
+      <BrowserRouter>
+        <HashRouter basename={"/"}>
+          <Routes>
+            <Route path="/" element={<WelcomePage playerName={playerName} setPlayerName={setPlayerName}/>}/>
+            <Route path="game" element={<Game setScore={setScore}/>}/>
+            <Route path="leaderboard" element={<Leaderboard username={playerName} score={score}/>}/>
+          </Routes>
+        </HashRouter>,
+      </BrowserRouter>
     </div>
   )
 }
