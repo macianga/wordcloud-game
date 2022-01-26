b01ba5af29eedbe782a5235289e0fcc4bc71f024
maciek <macieq2048@gmail.com>
2022-01-26 20:53:01 +0100

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css'
 import Game from "./views/Game";
+import { Routes, Route, Link } from "react-router-dom";
+import MainPage from "./views/MainPage";
+import Leaderboard from "./views/Leaderboard";
+import {useReducer, useState} from "react";
 
 function App() {
   return (
     <div className="App">
-      <Game />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="game" element={<Game />} />
+        <Route path="leaderboard" element={<Leaderboard username={playerName} score={12}/>} />
+      </Routes>
     </div>
   )
 }
