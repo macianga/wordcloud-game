4684c23560f90aa032793e6792bc57bd76853414
maciek <macieq2048@gmail.com>
2022-01-27 00:50:24 +0100

    cleanup from deploy on github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Game from "./views/Game";
-import {Routes, Route, BrowserRouter, HashRouter} from "react-router-dom";
+import {Routes, Route, BrowserRouter} from "react-router-dom";
 import WelcomePage from "./views/WelcomePage";
 import Leaderboard from "./views/Leaderboard";
 import {useState} from "react";
@@ -11,13 +11,13 @@ function App() {
 
   return (
     <div className="App">
-        <HashRouter basename={"/"}>
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<WelcomePage playerName={playerName} setPlayerName={setPlayerName}/>}/>
             <Route path="game" element={<Game setScore={setScore}/>}/>
             <Route path="leaderboard" element={<Leaderboard username={playerName} score={score}/>}/>
           </Routes>
-        </HashRouter>
+        </BrowserRouter>
     </div>
   )
 }
