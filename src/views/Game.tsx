1018cfd7314bc928f2033831c916eeec951a9c54
maciek <macieq2048@gmail.com>
2022-01-26 21:06:20 +0100

    calculate score

--- src/views/Game.tsx
+++ src/views/Game.tsx
@@ -41,13 +41,18 @@ function Game({setScore}: Props) {
     }
   }
 
+  const calculateScore = () =>{
+    // (liczba zaznaczonych poprawnych odpowiedzi * 2) - (liczba zaznaczonych błędnych odpowiedzi +
+    // liczba niezaznaczonych poprawnych odpowiedzi).
+    return selectedElements.reduce((accumulator, selectedElement) => {
+      const pointsEarned = goodWords.includes(selectedElement) ? 2 : -1;
+      return accumulator + pointsEarned;
+    }, 0)
+  }
+
   const finishGame = () => {
     setGameFinished(true);
-    const score = selectedElements.reduce((accumulator, selectedElement)=>{
-      const pointsAdded = goodWords.includes(selectedElement) ? 2 : -1;
-      return accumulator + pointsAdded;
-    },0);
-    setScore(score);
+    setScore(calculateScore());
   }
 
   const isWordSelected = (word: String) => {
