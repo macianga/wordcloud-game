e81f233c70cd67ee3195532c10846f9ad2215039
maciek <macieq2048@gmail.com>
2022-01-27 01:32:23 +0100

    change h1 to span

--- src/views/Leaderboard.tsx
+++ src/views/Leaderboard.tsx
@@ -25,18 +25,18 @@ function Leaderboard({username, score}: Props) {
     <div className="App">
       <div className="flex items-center justify-center min-h-screen pb-40">
         <div className="grid grid-cols-1">
-          <h1 className="text-center font-bold text-5xl mb-4">
+          <span className="text-center font-bold text-5xl mb-4">
             Congratulations, {username}!
-          </h1>
-          <h1 className="text-center text-4xl mb-4">
+          </span>
+          <span className="text-center text-4xl mb-4">
             Your score:
-          </h1>
-          <h1 className="text-center text-4xl mb-4 text-cyan-500">
+          </span>
+          <span className="text-center text-4xl mb-4 text-cyan-500">
             ✨{score} points✨
-          </h1>
-          <h1 className="text-center text-xl mb-4 text-purple-600">
+          </span>
+          <span className="text-center text-xl mb-4 text-purple-600">
             {getScoreMessage()}
-          </h1>
+          </span>
           <Link
             className="m-auto mt-2 p-2 pl-5 pr-5 border-2 border-cyan-600 rounded-md text-cyan-600 w-fit
                   hover:border-cyan-500 hover:text-cyan-500 transition-all font-bold text-2xl"
